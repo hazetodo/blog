d1a6c0c35ab1af523dbb2a5041026dbf3a540129
hazetodo <hazetodo@gmail.com>
2015-02-12 19:41:30 +0800

    add the view -- post new.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,25 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  def new
+    @post = Post.new
+  end
+
+  # def create
+  #   @post = Post.new(post_params)
+ 
+  #   if @post.save
+  #     # redirect_to post_path
+  #     redirect_to @post
+  #   else
+  #     render :new
+  #   end
+  # end
+ 
+  private
+ 
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
